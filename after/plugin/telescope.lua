c9028c8491e5764f6b4c34e3fe6761b215ef4e15
Tomas Boda <tomasboda.dev@gmail.com>
2023-01-18 23:58:25 +0100

    chng

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,6 +8,6 @@ require('telescope').setup({
   },
 })
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fe', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fe', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
