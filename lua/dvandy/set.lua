8b75ad0fb3784d54ead0253ce099efade44b5937
Tomáš Boďa <tomasboda.dev@gmail.com>
2023-12-26 19:40:36 +0100

    setters

--- lua/dvandy/set.lua
+++ lua/dvandy/set.lua
@@ -1,17 +1,20 @@
 vim.opt.relativenumber = false
 vim.opt.nu = true
 
-vim.opt.smartindent = true
+-- Indenting
+vim.opt.smartindent = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.smarttab = false
+
+-- Other
 vim.opt.termguicolors = true
 vim.opt.hidden = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
 vim.opt.cursorline = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.expandtab = false
-vim.opt.shiftwidth = 5
-vim.opt.smarttab = false
 vim.opt.encoding = "utf8"
 vim.opt.laststatus = 2
 vim.opt.scrolloff = 10
@@ -19,3 +22,4 @@ vim.opt.mouse = "a"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.wrap = false
+
